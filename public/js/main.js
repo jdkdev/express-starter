3d36900fe1c9682fb4e247d68a3776133494f2f1
Jordan <knightworks@pm.me>
2019-01-25 16:00:45 -0600

    backup

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,26 @@
-var app = new Vue({
-    el: "#app",
+new Vue({
+    el: '#app',
     data: {
-        message: 'hello'
-    },
-    created() {
+        message: 'hello there',
+        scss: 'h1 { color: red; > small { color: pink }}',
+        css: 'color: red',
     },
     filters: {
     },
+    created() {
+        this.convert()
+    }
     computed: {
     },
     methods: {
+        convert() {
+            axios.get('http://localhost:3000/convert?scss=' + encodeURIComponent(this.scss))
+            .then(res => {
+                console.log(res.data)
+                this.css = res.data.css
+                styles = document.getElementById('preview')
+                styles.innerHTML = this.css
+            })
+        }
     },
 })
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const fs = require("fs")
 const path = require('path')
 
 // Create Express App
 const app = express()
+
+app.use(cors())
 app.use(express.static('public'))
 
 //
